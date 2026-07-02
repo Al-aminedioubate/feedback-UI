eae2faabfbfaa0e9eb823ecbd205fb5063902d54
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-07-02 12:50:07 -0400

    debut d'evenement btn review

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let neutralBtn = document.getElementById("neutralBtn");
 let satisBtn = document.getElementById("SatisfiedBtn");
 
 let reviewBtn = document.getElementById("reviewBtn");
+
+
+
+//ajoutons l'evenement sur notre button send review
