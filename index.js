401aae882eb083bf770b170304d8101579d80ccb
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-07-06 22:27:55 -0400

    comment in the js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let sadBtn = document.getElementById("unhappyBtn");
 let neutralBtn = document.getElementById("neutralBtn");
 let satisBtn = document.getElementById("SatisfiedBtn");
 
+//review btn
 let reviewBtn = document.getElementById("reviewBtn");
 
 //ajoutons l'evenement sur notre button send review
