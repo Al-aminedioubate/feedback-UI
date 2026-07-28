2e5e8a5970172bb80af6e89d862934e53e42d0b9
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-07-27 22:40:40 -0400

    better days

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ let reviewBtn = document.getElementById("reviewBtn");
 
 //ajoutons l'evenement sur notre button send review
 reviewBtn.addEventListener("click", () => {});
+
+//don't worry tomorrow will be better
