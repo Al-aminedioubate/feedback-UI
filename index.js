3dc35976cd9a8f4784db315cd366f7bbd208d94c
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-08-11 22:45:59 -0400

    pas a pas

--- index.js
+++ index.js
@@ -12,3 +12,5 @@ reviewBtn.addEventListener("click", () => {});
 //le plus important de faire ce qu'on doit faire cest tout
 
 //Actuellement tres interresser par le reseau
+
+//j'avance bien en reseau cest magnefique
