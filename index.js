7733fe272fab2c959f05ec71134723fc34e1faa0
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-08-17 23:41:30 -0400

    cmd

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ let reviewBtn = document.getElementById("reviewBtn");
 
 //ajoutons l'evenement sur notre button send review
 reviewBtn.addEventListener("click", () => {});
+
+//on va chercher un peu
