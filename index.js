d76e9d553a7cfc811883dd3dd42cf1ec6147bf49
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-08-23 23:02:09 -0400

    ecran

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ let reviewBtn = document.getElementById("reviewBtn");
 
 //ajoutons l'evenement sur notre button send review
 reviewBtn.addEventListener("click", () => {});
+
+//La prochaine chose de ce mois cest mon ecran
