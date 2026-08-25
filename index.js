137aaa292940ba10813a7b2d6d049d4183b9a3cb
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-08-25 15:19:46 -0400

    effacer l'ecran

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ let reviewBtn = document.getElementById("reviewBtn");
 //ajoutons l'evenement sur notre button send review
 reviewBtn.addEventListener("click", () => {});
 
-//La prochaine chose de ce mois cest mon ecran
-
 //Apres les claviers et souris
 
 //Termine la formation en reseau et commencer react native
