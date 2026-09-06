61f8126feafc4802138affa514a1a7a3d7ec5e71
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-09-06 19:25:41 -0400

    too late for that

--- index.js
+++ index.js
@@ -52,3 +52,5 @@ reviewBtn.addEventListener("click", () => {});
 //La base compris
 
 //i gave my heart and my best to someone who did value it
+
+//Then i took it back so now the person wants come back but it's late now
